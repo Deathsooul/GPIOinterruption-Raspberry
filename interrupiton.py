011e7cbd881ad0087b54842122d164aaf31aa36e
Wallace <wbescrovaine@gmail.com>
2020-12-15 13:14:50 -0300

    Working

--- interrupiton.py
+++ interrupiton.py
@@ -1,39 +1,27 @@
-#!/usr/bin/env python2.7  
-# script by Alex Eames https://raspi.tv  
-  
 import RPi.GPIO as GPIO  
 GPIO.setmode(GPIO.BCM)  
+
+# Configurando pino como input de borda de decida
+GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  
   
-# GPIO 23 & 24 set up as inputs. One pulled up, the other down.  
-# 23 will go to GND when button pressed and 24 will go to 3V3 (3.3V)  
-# this enables us to demonstrate both rising and falling edge detection  
-GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)  
+# GPIO 24 set up as an input, pulled down, connected to 3V3 on button press  
 GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  
   
-# now we'll define the threaded callback function  
-# this will run in another thread when our event is detected  
+# threaded de callback
+# Chama a função quando callback quando um evento é detectado
 def my_callback(channel):  
-    print "Rising edge detected on port 24 - even though, in the main thread,"  
-    print "we are still waiting for a falling edge - how cool?\n"  
-  
-print "Make sure you have a button connected so that when pressed"  
-print "it will connect GPIO port 17 (pin 16) to GND (pin 6)\n"  
-print "You will also need a second button connected so that when pressed"  
-print "it will connect GPIO port 24 (pin 18) to 3V3 (pin 1)"  
-raw_input("Press Enter when ready\n>")  
-  
-# The GPIO.add_event_detect() line below set things up so that  
-# when a rising edge is detected on port 24, regardless of whatever   
-# else is happening in the program, the function "my_callback" will be run  
-# It will happen even while the program is waiting for  
-# a falling edge on the other button.  
-GPIO.add_event_detect(24, GPIO.RISING, callback=my_callback)  
-  
+    print ("Faz o que tu quiser")  
+
+
+# Adicionando evento referente ao pino selecionado
+GPIO.add_event_detect(17, GPIO.FALLING, callback=my_callback, bouncetime=300)  
+
+
+# Esperando o acontecimento de um evento para que o programa nao pare
 try:  
-    print "Waiting for falling edge on port 17"  
-    GPIO.wait_for_edge(17, GPIO.FALLING)  
-    print "Falling edge detected. Here endeth the second lesson."  
+    print "Waiting for rising edge on port 24"  
+    GPIO.wait_for_edge(24, GPIO.RISING)  
+    print "Rising edge detected on port 24. Here endeth the third lesson."  
   
 except KeyboardInterrupt:  
-    GPIO.cleanup()       # clean up GPIO on CTRL+C exit  
-GPIO.cleanup()           # clean up GPIO on normal exit  
+    pass
